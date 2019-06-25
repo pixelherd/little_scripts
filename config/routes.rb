f187a8a97512fcb76e2f07f3d5ee3a859c0d4d56
pixelherd <6398412+pixelherd@users.noreply.github.com>
2019-06-25 16:52:13 +0100

    add devise for Tester model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :testers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'pages#index'
+
+  resources :testers, only: :index
+
 end
