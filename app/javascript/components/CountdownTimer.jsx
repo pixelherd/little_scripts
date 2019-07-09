db5f11ff8799c0792595679e2f34c22496bc1b71
pixelherd <6398412+pixelherd@users.noreply.github.com>
2019-07-09 21:58:19 +0100

    implement slide timers :) :)

--- app/javascript/components/CountdownTimer.jsx
+++ app/javascript/components/CountdownTimer.jsx
@@ -1,20 +1,14 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import "./countdown_timer.scss"
 const CountdownTimer = ({time}) =>  {
 
-    const [timeRemaining, setTimeRemaining] = React.useState(time);
 
-    function tick() {
-        setTimeRemaining(timeRemaining => timeRemaining - 1)
-    }
 
     const minutes = Math.floor(time / 60);
     const seconds = Math.floor(time % 60);
 
     return (<div className="CountdownTimer">
         <div>
-            <p><button className="fakeTick" onClick={tick}> decrease seconds </button> </p>
-            {/*<p> {timeRemaining}</p>*/}
             <p>{minutes}:{seconds}</p>
 
         </div>
--- app/javascript/components/StepCard.jsx
+++ app/javascript/components/StepCard.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import StepName from './StepName';
 import CountdownTimer from "./CountdownTimer";
 import "./card.scss";
@@ -14,10 +14,28 @@ export const StepCard = (props) => {
     let hidden = props.isActive ? '' : 'hidden';
     const fullName = [props.className, hidden].join(" ");
 
+    const [timeRemaining, setTimeRemaining] = useState(props.time);
+
+    useEffect(
+        () => {
+            if (timeRemaining > 0 && props.isActive) {
+                let timer = setTimeout(
+                    () => {
+                        setTimeRemaining(timeRemaining => timeRemaining - 1)
+                    }, 1000);
+                return () => clearTimeout(timer)
+            }
+        },
+        // if either of these values changes, clear the timer; otherwise
+        // either the timers on all slides will update at once, or they don't update
+        // after the first render:
+        [timeRemaining, props.isActive]
+    );
+
     return (
             <section className={fullName}>
                 <StepName step={props.step}/>
-                <CountdownTimer time={props.time}/>
+                <CountdownTimer time={timeRemaining}/>
             </section>
     )
 };
