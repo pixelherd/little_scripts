4dac4bc6ec3d30f99ca713d7073c6813b99e1a7a
pixelherd <6398412+pixelherd@users.noreply.github.com>
2019-07-09 22:08:36 +0100

    add leading zeroes to countdown clock

--- app/javascript/components/CountdownTimer.jsx
+++ app/javascript/components/CountdownTimer.jsx
@@ -1,18 +1,22 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import "./countdown_timer.scss"
 const CountdownTimer = ({time}) =>  {
-
-
 
     const minutes = Math.floor(time / 60);
     const seconds = Math.floor(time % 60);
 
     return (<div className="CountdownTimer">
         <div>
-            <p>{minutes}:{seconds}</p>
+            <p>{formatTime(minutes)}:{formatTime(seconds)}</p>
 
         </div>
     </div>)
 };
 
 export default CountdownTimer
+
+function formatTime(unit){
+    const leadingZero = unit < 10 ? '0' : '';
+
+    return leadingZero + unit.toString()
+}
