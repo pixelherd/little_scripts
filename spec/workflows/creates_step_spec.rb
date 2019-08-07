110bc0048d3bbfbbc4be578d0425a835989156c3
pixelherd <6398412+pixelherd@users.noreply.github.com>
2019-08-07 18:23:20 +0100

    add CreatesStep test

---
+++ spec/workflows/creates_step_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+require 'rails_helper'
+RSpec.describe "CreatesStep" do
+
+  describe "initialization" do
+
+    it "creates a Step given valid params" do
+      valid_script = FactoryBot.build_stubbed(:little_script_with_steps)
+      creator = CreatesStep.new(valid_script, name: "Hoover the dog", duration_hours: 0, duration_minutes: 5)
+      new_step = creator.make_step
+      expect(new_step.name).to eq("Hoover the dog")
+      expect(new_step.duration).to eq(300)
+    end
+
+    it "attaches Script to a valid LittleScript" do
+      valid_script = FactoryBot.build_stubbed(:little_script_with_steps)
+      creator = CreatesStep.new(valid_script, name: "Hoover the dog", duration_hours: 0, duration_minutes: 5)
+      new_step = creator.make_step
+      expect(new_step.little_script).to eq(valid_script)
+      expect(new_step).to be_valid
+    end
+
+  end
+
+end
